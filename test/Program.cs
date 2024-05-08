c8225652df04c5cb3607af63113541f5f2fc10b2
Hus1203 <115139049+Hus1203@users.noreply.github.com>
2024-05-08 21:44:40 +0300

    Это программа, для теста логгера (прочти описание)
    В тестовой программе я просто указываю, что подключаю логгер как библиотеку

---
+++ test/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using Log;
+
+
+namespace logger
+{
+    public class Program 
+    {
+        static void Main(string[] args) // Тестовая программа 
+        {
+            Logger logger = Logger.Instance; // Создания синглтон экземпляра
+
+            // Опиции
+            logger.level = Logger.Level.ERROR;
+            logger.Writemode = Logger.Mode.CONSOLE_FILE;
+            logger.SeparateLogging = true;
+            logger.NewFile = true;
+
+            // Запись логов в "название проекта"\bin\Debug
+            logger.LOGD("Debug Hello");
+            logger.LOGI("Info Hello");
+            logger.LOGW("Warn Hello");
+            logger.LOGE("Error Hello");
+
+            Console.ReadKey();
+        }
+    }
+}
